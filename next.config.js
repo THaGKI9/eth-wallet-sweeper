18a3d35d5f2c3f7e9dd20ec2be93e87a8c970e35
Tony Ke <thagki9@outlook.com>
2022-04-01 02:04:15 +0800

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+function getAssetPrefix() {
+  if (process.env.GITHUB_ACTIONS === 'true') {
+    const [, repo] = process.env.GITHUB_REPOSITORY.split('/');
+    return `/${repo.toLowerCase()}/`;
+  }
+
+  return '';
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  productionBrowserSourceMaps: true,
+  assetPrefix: getAssetPrefix(),
+  env: {
+    VERSION: process.env.VERSION,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,283 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { init, useConnectWallet, useSetChain } from '@web3-onboard/react';
+import injectedModule from '@web3-onboard/injected-wallets';
+import ledgerModule from '@web3-onboard/ledger';
+import walletConnectModule from '@web3-onboard/walletconnect';
+import walletLinkModule from '@web3-onboard/walletlink';
+import { BigNumber, ethers } from 'ethers';
+import { Container, Text, Button, Input, Row, Spacer, Radio, useInput, Grid } from '@nextui-org/react';
+import moment from 'moment';
+
+init({
+  wallets: [injectedModule(), ledgerModule(), walletConnectModule(), walletLinkModule()],
+  chains: [
+    {
+      id: '0x1',
+      token: 'ETH',
+      label: 'Ethereum Mainnet',
+      rpcUrl: 'https://mainnet.infura.io/v3/b0ddbf6d18524aaf84f91b46fba9459f',
+    },
+    {
+      id: '0x3',
+      token: 'tROP',
+      label: 'Ethereum Ropsten Testnet',
+      rpcUrl: 'https://ropsten.infura.io/v3/b0ddbf6d18524aaf84f91b46fba9459f',
+    },
+    {
+      id: '0x4',
+      token: 'rETH',
+      label: 'Ethereum Rinkeby Testnet',
+      rpcUrl: 'https://rinkeby.infura.io/v3/b0ddbf6d18524aaf84f91b46fba9459f',
+    },
+    {
+      id: '0x89',
+      token: 'MATIC',
+      label: 'Matic Mainnet',
+      rpcUrl: 'https://matic-mainnet.chainstacklabs.com',
+    },
+  ],
+});
+
+let setCustomGasPriceByOnChainData = false;
 
 const Home: NextPage = () => {
+  const [{ wallet, connecting }, connect] = useConnectWallet();
+  const [{ connectedChain }] = useSetChain();
+  const version = `${(process.env.VERSION ?? 'dirty').substring(0, 8)}, ${moment().format('yyyy-MM-DD')}`;
+
+  const [currentWalletBalance, setCurrentWalletBalance] = useState<BigNumber>();
+  const [currentGasPrice, setCurrentGasPrice] = useState<BigNumber>();
+  const [useCustomGasPrice, setUseCustomGasPrice] = useState(false);
+  const [gasPrice, setGasPrice] = useState<BigNumber>();
+  const [gasFee, setGasFee] = useState<BigNumber>();
+  const [valueToTransfer, setValueToTransfer] = useState<BigNumber>();
+  const [executing, setExecuting] = useState(false);
+
+  const inputGasPrice = useInput('');
+  const inputRecipientAddress = useInput('0x42f30aa6d2237248638d1c74ddfcf80f4ecd340a');
+
+  const web3Provider = useMemo(() => {
+    let onboardProvider = wallet?.provider;
+    if (!onboardProvider) return;
+
+    return new ethers.providers.Web3Provider(onboardProvider as any);
+  }, [wallet]);
+
+  const updateCurrentWalletBalance = useCallback(async () => {
+    let walletAddress = wallet?.accounts[0].address;
+    if (!walletAddress) setCurrentWalletBalance(undefined);
+    if (!web3Provider) setCurrentWalletBalance(undefined);
+
+    web3Provider!.getBalance(walletAddress!).then((balance) => setCurrentWalletBalance(balance));
+  }, [wallet, web3Provider]);
+
+  const updateCurrentGasPrice = useCallback(async () => {
+    if (!web3Provider) {
+      setCurrentGasPrice(undefined);
+    }
+
+    web3Provider?.getGasPrice().then((gasPrice) => {
+      setCurrentGasPrice(gasPrice);
+      if (!useCustomGasPrice) setGasPrice(gasPrice);
+    });
+  }, [useCustomGasPrice, web3Provider]);
+
+  const executeTransfer = useCallback(async () => {
+    if (executing) {
+      return;
+    }
+    setExecuting(true);
+
+    try {
+      const signer = web3Provider?.getSigner(wallet?.accounts[0].address);
+      await signer?.sendTransaction({
+        to: inputRecipientAddress.value,
+        value: valueToTransfer,
+        gasLimit: 21000,
+        maxFeePerGas: gasPrice,
+        maxPriorityFeePerGas: gasPrice,
+      });
+    } finally {
+      setExecuting(false);
+    }
+  }, [executing, gasPrice, inputRecipientAddress.value, valueToTransfer, wallet?.accounts, web3Provider]);
+
+  useEffect(() => {
+    if (!web3Provider) return;
+
+    updateCurrentGasPrice();
+    const intervalIdUpdateCurrentGasPrice = setInterval(updateCurrentGasPrice, 10 * 1000);
+    const intervalIdUpdateCurrentWalletBalance = setInterval(updateCurrentWalletBalance, 1000);
+
+    return () => {
+      clearInterval(intervalIdUpdateCurrentGasPrice);
+      clearInterval(intervalIdUpdateCurrentWalletBalance);
+    };
+  }, [updateCurrentGasPrice, updateCurrentWalletBalance, web3Provider]);
+
+  useEffect(() => {
+    if (useCustomGasPrice) {
+      try {
+        setGasPrice(ethers.utils.parseUnits(inputGasPrice.value || '0', 'gwei'));
+      } catch (e) {
+        console.error('Invalid gas price:', e);
+        setGasPrice(undefined);
+      }
+    }
+  }, [inputGasPrice.value, useCustomGasPrice]);
+
+  useEffect(() => {
+    if (useCustomGasPrice && inputGasPrice.value === '' && !setCustomGasPriceByOnChainData) {
+      setCustomGasPriceByOnChainData = true;
+      inputGasPrice.setValue(ethers.utils.formatUnits(currentGasPrice || BigNumber.from(0), 'gwei'));
+    }
+  }, [currentGasPrice, inputGasPrice, useCustomGasPrice]);
+
+  useEffect(() => {
+    setGasFee(gasPrice ? gasPrice.mul(21000) : undefined);
+  }, [gasPrice]);
+
+  useEffect(() => {
+    setValueToTransfer(gasFee && currentWalletBalance ? currentWalletBalance.sub(gasFee) : undefined);
+  }, [currentWalletBalance, gasFee]);
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Ethereum Wallet Sweeper</title>
         <meta name="description" content="Generated by create next app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Container fluid css={{ paddingTop: '5rem', paddingBottom: '5rem' }}>
+        <Row align="baseline">
+          <Text h1 css={{ textGradient: '45deg, $blue500 -20%, $pink500 50%' }}>
+            Ethereum Wallet Sweeper
+          </Text>
+          <Text span css={{ paddingLeft: '1rem' }}>
+            (Version: {version})
+          </Text>
+        </Row>
+        <Spacer y={0.2} />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Row>
+          {!wallet && (
+            <Button ghost color="gradient" onClick={() => connect({})} disabled={connecting}>
+              {connecting ? 'Connecting...' : 'Connect'}
+            </Button>
+          )}
+        </Row>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        {wallet && (
+          <>
+            <Row>
+              <div>
+                <Text>Connected: {wallet.accounts[0]?.address}</Text>
+                <Text>Chain: {connectedChain ? Number.parseInt(connectedChain.id, 16) : ''}</Text>
+              </div>
+            </Row>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <Spacer y={1.5} />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <Row>
+              <Text h2>Setting</Text>
+            </Row>
+            <Row>
+              <Input
+                {...inputRecipientAddress.bindings}
+                label="Recipient Address"
+                width="42ch"
+                status={
+                  !inputRecipientAddress.value || ethers.utils.isAddress(inputRecipientAddress.value)
+                    ? 'default'
+                    : 'error'
+                }
+              />
+            </Row>
+            <Spacer y={0.5} />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <Row>
+              <Radio.Group initialValue={'auto-gas'} onChange={(e) => setUseCustomGasPrice(e === 'custom-gas')}>
+                <Radio size="sm" value="auto-gas">
+                  Auto Gas Price
+                  <Spacer x={1} />
+                  <Input
+                    readOnly
+                    disabled={useCustomGasPrice}
+                    labelRight="gwei"
+                    value={currentGasPrice ? `${ethers.utils.formatUnits(currentGasPrice, 'gwei')}` : 'N/A'}
+                  />
+                </Radio>
+                <Radio size="sm" value="custom-gas">
+                  Custom Gas Price
+                  <Spacer x={1} />
+                  <Input
+                    {...inputGasPrice.bindings}
+                    status={gasPrice && gasFee?.lte(currentWalletBalance || gasPrice) ? 'default' : 'error'}
+                    labelRight="gwei"
+                    disabled={!useCustomGasPrice}
+                  />
+                </Radio>
+              </Radio.Group>
+            </Row>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            <Spacer y={0.5} />
+            <Row>
+              <Text h2> Calculation</Text>
+            </Row>
+            <Row>
+              <Container fluid>
+                <Grid.Container>
+                  <Grid xs={1}>Balance:</Grid>
+                  <Grid xs>{currentWalletBalance ? `${ethers.utils.formatEther(currentWalletBalance)}Ξ` : 'N/A'}</Grid>
+                </Grid.Container>
+                <Grid.Container>
+                  <Grid xs={1}>Gas Usage:</Grid>
+                  <Grid xs>21000</Grid>
+                </Grid.Container>
+                <Grid.Container>
+                  <Grid xs>----------------------</Grid>
+                </Grid.Container>
+                <Grid.Container>
+                  <Grid xs={1}>Gas Fee:</Grid>
+                  <Grid xs>{gasFee ? `${ethers.utils.formatEther(gasFee)}Ξ` : 'N/A'}</Grid>
+                </Grid.Container>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
-}
+                <Grid.Container>
+                  <Grid xs={1}>Transfer:</Grid>
+                  <Grid xs>
+                    {valueToTransfer && gasFee && !gasFee.isNegative() && !valueToTransfer.isNegative()
+                      ? `${ethers.utils.formatEther(valueToTransfer)}Ξ`
+                      : 'N/A'}
+                  </Grid>
+                </Grid.Container>
+              </Container>
+            </Row>
 
-export default Home
+            <Spacer y={0.5} />
+            <Row>
+              <Button
+                color="gradient"
+                auto
+                disabled={
+                  executing ||
+                  !(
+                    valueToTransfer &&
+                    !valueToTransfer.isNegative() &&
+                    ethers.utils.isAddress(inputRecipientAddress.value)
+                  )
+                }
+                onClick={() => executeTransfer()}
+              >
+                {executing ? 'Sweep...' : 'Sweep!'}
+              </Button>
+            </Row>
+          </>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default Home;
